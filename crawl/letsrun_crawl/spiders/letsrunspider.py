31f4ac10a559b5f94db9afe04d6539861d4706d7
Chris Green <cmgreen210@gmail.com>
2015-05-22 11:15:24 -0700

    update item parsing

--- crawl/letsrun_crawl/spiders/letsrunspider.py
+++ crawl/letsrun_crawl/spiders/letsrunspider.py
@@ -45,22 +45,25 @@ class LetsRunSpider(CrawlSpider):
                 './/*[@class="author"]//text()')[0].extract()
 
             item["post_text"] = post.xpath(
-                './/span[contains(@id, "intelliTXT")]//text()')[0].extract()
+                './/span[contains(@id, "intelliTXT")]/text()')[0].extract()
 
-            subject_xpath = './/span[contains(@class, "subject_line")]//text()'
+            subject_xpath = './/span[contains(@class, "subject_line")]/text()'
             item['subject'] = post.xpath(subject_xpath)[0].extract()
 
             item['timestamp'] = post.xpath(
-                './/span[contains(@class, "timestamp")]//text()')[0].extract()
+                './/span[contains(@class, "timestamp")]/text()')[0].extract()
 
             reply_to = post.xpath(
                 './/span[contains(@class,"in_reply_to")]/a//text()')
-            item['reply_to_author'] = None if len(reply_to) == 0 else reply_to[0].extract()
+
+            item['reply_to_author'] = None \
+                if len(reply_to) == 0 else reply_to[0].extract()
 
             reply_url = post.xpath(
                 './/span[contains(@class,"in_reply_to")]/a//@href')
 
-            item['in_reply_url'] = None if len(reply_url) == 0 else reply_url[0].extract()
+            item['in_reply_url'] = None \
+                if len(reply_url) == 0 else reply_url[0].extract()
 
             items.append(item)
         return items
